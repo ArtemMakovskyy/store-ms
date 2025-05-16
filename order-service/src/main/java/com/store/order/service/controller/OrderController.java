37b60e7ea7b045d59ae9dd0d4cce24b7f66b264a
Artem Makovskyy <photomak79@gmail.com>
2025-05-16 15:44:04 +0300

    order-service Circuit Breaker implemented

--- order-service/src/main/java/com/store/order/service/controller/OrderController.java
+++ order-service/src/main/java/com/store/order/service/controller/OrderController.java
@@ -2,7 +2,12 @@ package com.store.order.service.controller;
 
 import com.store.order.service.dto.OrderRequest;
 import com.store.order.service.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,12 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
 
     @PostMapping
+    @CircuitBreaker(name = "inventory", fallbackMethod = "placeOrderFallbackMethod")
     public ResponseEntity<String> placeOrder(@RequestBody OrderRequest orderRequest) {
         return ResponseEntity.ok(orderService.placeOrder(orderRequest));
+    }
+
+    public ResponseEntity<String> placeOrderFallbackMethod(
+            OrderRequest orderRequest, Throwable throwable) {
+        log.error("Cannot Place Order, executing fallback logic", throwable);
+        return ResponseEntity.badRequest().body("Cannot place order, please try again later.");
     }
 }
