afd54476f862b1b0d3072f1ae214e0df30d2681a
Artem Makovskyy <photomak79@gmail.com>
2025-05-19 17:34:05 +0300

    implemented resilience4j in order

--- order-service/src/main/java/com/store/order/service/controller/OrderController.java
+++ order-service/src/main/java/com/store/order/service/controller/OrderController.java
@@ -2,8 +2,13 @@ package com.store.order.service.controller;
 
 import com.store.order.service.dto.OrderRequest;
 import com.store.order.service.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
--- order-service/src/main/java/com/store/order/service/service/OrderService.java
+++ order-service/src/main/java/com/store/order/service/service/OrderService.java
@@ -6,23 +6,26 @@ import com.store.order.service.model.Order;
 import com.store.order.service.repository.OrderRepository;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final InventoryRestClient inventoryClient;
+    private final InventoryRestClient inventoryRestClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        boolean inStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        boolean inStock = inventoryRestClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
         if (inStock) {
             var order = mapToOrder(orderRequest);
             orderRepository.save(order);
         } else {
+            log.error("Product with SKU code {} is not in stock", orderRequest.skuCode());
             throw new RuntimeException("Product with Skucode " + orderRequest.skuCode() + "is not in stock");
         }
     }
